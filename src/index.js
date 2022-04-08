fb62549c7e92f2f93e85bf46a53016a2f5514455
Keyvan Alonso <keyvanalonso@gmail.com>
2022-04-08 00:05:24 -0400

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // Form object
+  const formGrab = document.querySelector('#create-task-form')
+
+  // Adds items to lists
+  formGrab.addEventListener('submit', (e) => {
+    e.preventDefault()
+    // Grabs value of to-do
+    lister(e.target.newtaskdescription.value)
+  })
+
 });
+
+// Creates element to add
+function lister(task){
+  const elementToAdd = document.createElement('p')
+  const elementTxt = task
+
+  const xButton = document.createElement('button')
+  const xButtTxt = 'X'
+  xButton.textContent = xButtTxt
+
+  // Deletes text and button of clicked
+  xButton.addEventListener('click', (e) => {
+    e.target.parentNode.remove()
+  })
+
+  elementToAdd.textContent = `${elementTxt} `
+  elementToAdd.appendChild(xButton)
+  
+  document.querySelector('#tasks').appendChild(elementToAdd)
+}
